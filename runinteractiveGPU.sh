e8306c1ee1f79de489ef6a9422e3d93176177f76
Jonathan Sullivan <jesully83@gmail.com>
2023-09-10 12:04:48 -0400

    updates

--- runinteractiveGPU.sh
+++ runinteractiveGPU.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun --partition=gpu --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --gres=gpu:v100-sxm2:1 --mem=8Gb --time=00:30:00 --export=ALL --pty /bin/bash
+srun --partition=gpu --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --gres=gpu:p100:1 --mem=8Gb --time=08:00:00 --export=ALL --pty /bin/bash
--- startup.sh
+++ startup.sh
@@ -3,3 +3,5 @@
 module load git
 module load cuda/11.8
 module load gcc/11.1.0
+module load opencv/3.4.3
+module load cmake/3.23.2
